fe9ee23fafbad5aa00086e2765f222baea54ae56
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-04-18 18:17:18 +0500

    7-18.04: created animation for fragment adding & removing

---
+++ app/src/main/java/com/example/a7_animation_scroll/AnimationFragment.kt
@@ -0,0 +1,19 @@
+package com.example.a7_animation_scroll
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+
+class AnimationFragment : Fragment(R.layout.animation_fragment) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/a7_animation_scroll/MainActivity.kt
+++ app/src/main/java/com/example/a7_animation_scroll/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.a7_animation_scroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        startButton.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.setCustomAnimations(
+                R.anim.fragment_animation,
+                R.anim.backwards_fragment_animation,
+                0,
+                R.anim.backwards_fragment_animation)
+            transaction.add(R.id.fragment_container, AnimationFragment(), "animation_fragment")
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
